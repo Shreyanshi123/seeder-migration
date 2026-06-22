b1ba2592f08ff84cf3c0824963a17e3b1e4cae32
Shreyanshi123 <85137961+Shreyanshi123@users.noreply.github.com>
2026-06-22 11:31:16 +0530

    Add files via upload

---
+++ SearchLego.DataSeeder.MongoDB.Host/IConfigUpdateRepsitory.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using SearchLego.DataSeeder.Common;
+using System.Collections.Generic;
+
+namespace SearchLego.DataSeeder.MongoDB.Host
+{
+    public interface IConfigUpdateRepsitory : IRepository<BsonDocument>
+    {
+        IList<BsonDocument> GetAll();
+    }
+}
---
+++ SearchLego.DataSeeder.MongoDB.Host/IMongoModelConfigRepository.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace SearchLego.DataSeeder.MongoDB.Host
+{
+    public interface IMongoModelConfigRepository 
+    {
+        BsonDocument GetModelConfig(int clientId, int projectId, string jobName, bool isLatest);
+    }
+}
+
