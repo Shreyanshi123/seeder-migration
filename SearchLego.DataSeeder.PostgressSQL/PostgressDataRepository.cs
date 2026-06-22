db4a49dad53c2c6d32c0386fb716d7fb33492dee
Shreyanshi123 <85137961+Shreyanshi123@users.noreply.github.com>
2026-06-22 11:33:04 +0530

    Add files via upload

---
+++ SearchLego.DataSeeder.PostgressSQL/PostgressDataRepository.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using SearchLego.DataSeeder.Common;
+using SearchLego.DataSeeder.Host;
+using System;
+using System.Data;
+
+namespace SearchLego.DataSeeder.PostgressSQL
+{
+    public class PostgressDataRepository : IDataSourceFactory
+    {
+        private readonly string _connectionString;
+
+        public PostgressDataRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public string GetData(string Query, string DefaultValue)
+        {
+            Query = Query.IndexOf("(") > 0 ? Query + ")" : Query;  // this is for temp solution 
+            using var _postgressHelper = new PostgressHelper(_connectionString);
+            DataSet objDs = _postgressHelper.ExecDataSet(Query);
+            if (objDs != null && objDs.Tables.Count > 0 && objDs.Tables[0].Rows.Count > 0)
+                return JsonConvert.SerializeObject(CommonUtility.ConvertDataTableFieldType(objDs.Tables[0], DefaultValue));
+            return string.Empty;
+        }
+
+        public DataSet GetDataSet(string Query)
+        {
+            throw new NotImplementedException();
+        }
+        public string GetData(string query, string defaultValue, string connectionString)
+        {
+            query = query.IndexOf("(") > 0 ? query + ")" : query;  // this is for temp solution 
+            using var _postgressHelper = new PostgressHelper(connectionString);
+            DataSet objDs = _postgressHelper.ExecDataSet(query);
+            if (objDs != null && objDs.Tables.Count > 0 && objDs.Tables[0].Rows.Count > 0)
+                return JsonConvert.SerializeObject(CommonUtility.ConvertDataTableFieldType(objDs.Tables[0], defaultValue));
+            return string.Empty;
+        }
+    }
+}
---
+++ SearchLego.DataSeeder.PostgressSQL/PostgressHelper.cs
@@ -0,0 +1,101 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+
+namespace SearchLego.DataSeeder.PostgressSQL
+{
+
+    public sealed class PostgressHelper : IDisposable
+    {
+        // Internal members
+        private string _connString = null;
+        private NpgsqlConnection _conn = null;
+        private bool _disposed = false;
+
+        /// <summary>
+        /// Sets or returns the connection string use by all instances of this class.
+        /// </summary>
+
+        /// <summary>
+        /// Returns the current SqlTransaction object or null if no transaction
+        /// is in effect.
+        /// </summary>
+
+
+        /// <summary>
+        /// Constructure using connection string override
+        /// </summary>
+        /// <param name="connString">Connection string for this instance</param>
+        public PostgressHelper(string connString)
+        {
+            _connString = connString;
+            Connect();
+        }
+
+        // Creates a SqlConnection using the current connection string
+        private void Connect()
+        {
+            _conn = new NpgsqlConnection(_connString);
+            _conn.Open();
+        }
+
+
+        #region Exec Members
+
+
+        /// <summary>
+        /// Executes a query and returns the results as a DataSet
+        /// </summary>
+        /// <param name="qry">Query text</param>
+        /// <param name="args">Any number of parameter name/value pairs and/or SQLParameter arguments</param>
+        /// <returns>Results as a DataSet</returns>
+        public DataSet ExecDataSet(string qry)
+        {
+            using var cmd = new NpgsqlDataAdapter(qry, _connString);
+            DataSet dbset = new DataSet();
+            cmd.Fill(dbset);
+            return dbset;
+        }
+
+
+        #endregion
+
+        #region Transaction Members
+
+        /// <summary>
+        /// Begins a transaction
+        /// </summary>
+        /// <returns>The new SqlTransaction object</returns>
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                // Need to dispose managed resources if being called manually
+                if (disposing)
+                {
+                    if (_conn != null)
+                    {
+                        //Rollback();
+                        _conn.Close();
+                        _conn.Dispose();
+                        _conn = null;
+                    }
+                }
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+
+}
